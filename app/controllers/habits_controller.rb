7355b8d7e9e2f6fde7a69047ae0a1d23fb85f01a
Mari-8 <ryanaltimari8@gmail.com>
2020-12-22 22:10:45 -0600

    Added protection to patch route, cleaned up unnecessary instance variables

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -15,13 +15,13 @@ class HabitsController < ApplicationController
 
     post '/habits' do 
 
-        @habit = Habit.new(name: params[:name], why: params[:why], when: params[:when], where: params[:where], duration: params[:duration], routine_id: session[:routine_id], user_id: session[:user_id])
+        habit = Habit.new(name: params[:name], why: params[:why], when: params[:when], where: params[:where], duration: params[:duration], routine_id: session[:routine_id], user_id: session[:user_id])
         
-        if @habit.save 
+        if habit.save 
             flash[:message] = "You have added a habit! keep it up!"
             redirect "/routines/#{ session[:routine_id]}"
         else 
-            flash[:error] = "Habit creation failed: #{@habit.errors.full_messages.to_sentence}"
+            flash[:error] = "Habit creation failed: #{habit.errors.full_messages.to_sentence}"
             redirect "/habits/new" 
         end 
         
@@ -53,9 +53,9 @@ class HabitsController < ApplicationController
     end 
 
     patch '/habits/:id' do 
-        @habit = Habit.find_by_id(params[:id])
-        if can_edit_habit(@habit) 
-            @habit.update(params["habit"])
+        habit = Habit.find_by_id(params[:id])
+        if can_edit_habit(habit) 
+            habit.update(params["habit"])
             redirect '/habits'
         else 
            flash[:error] = "Cannot edit another users habit."
@@ -64,9 +64,9 @@ class HabitsController < ApplicationController
     end 
 
     delete '/habits/:id' do 
-        @habit = Habit.find_by_id(params[:id])
+        habit = Habit.find_by_id(params[:id])
         if can_edit_habit(@habit) 
-            @habit.destroy
+            habit.destroy
             redirect '/habits'
         else 
             flash[:error] = "Cannot delete another users habit" 
